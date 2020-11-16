2cb0b18f62990039eec3b7dab79fd5b51986d7a4
RODRIGO <artoapanta3@espe.edu.ec>
2020-11-15 21:29:43 -0500

    C#

---
+++ Programa2P/Programa2P/Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+
+namespace Programa2P
+{
+    class Program
+    {
+        protected static int origRow;
+        protected static int origCol;
+       static int drawWave(int frecuencia,int amplitud)//Dibuja ondas dependiendo de la frecuencia que ingrese en la funcion
+        {
+            int a = 3;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            for (int o = 0; o < frecuencia; o++)
+            {
+                for (int i = 0; i <= amplitud; i++)
+                {
+                    position(25, a);
+                    for (int j = 0; j < i; j++)
+                    {
+                        Console.Write(i);
+                    }
+                    a++;
+                    Console.Write("\n");
+                }
+                for (int i = amplitud - 1; i > 0; i--)
+                {
+                    position(25, a);
+                    for (int j = 0; j < i; j++)
+                    {
+                        
+                        Console.Write(i);
+                    }
+                    a++;
+                    Console.Write("\n");
+                }
+                Console.Write("\n");
+            }
+            return a;
+        }
+        protected static void WriteAt(string s, int x, int y)//Se coloca en un punto en especifico de la consola y dibuja un caracter
+        {
+            try
+            {
+                Console.SetCursorPosition(origCol + x, origRow + y);
+                Console.Write(s);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.Clear();
+                Console.WriteLine(e.Message);
+            }
+        }
+        protected static void position(int x, int y)//Nos coloca en una posicion de la consola y desde ese punto podemos escribir 
+        {
+            try
+            {
+                Console.SetCursorPosition(origCol + x, origRow + y);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.Clear();
+                Console.WriteLine(e.Message);
+            }
+        }
+        static void margin()//Crea un margen donde se desarrollara la mayoria del programa 
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+           
+            for (int i = 0; i < 50; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    if (i == 0 || j == 0 || j == 19 || i == 49)
+                    {
+                        WriteAt("=", i, j);
+                    }
+                    Console.WriteLine("\n");
+                }
+            }
+        }
+        static void wavemargin(int tam)//Crea un margen en especifico donde se dibujara la onda 
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            for (int i = 0; i < 50; i++)
+            {
+                for (int j = 0; j < tam; j++)
+                {
+                    if (i == 0 || j == 0 || j == tam-1 || i == 49)
+                    {
+                        WriteAt("=", i, j);
+                    }
+                    Console.WriteLine("\n");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            origRow = Console.CursorTop;
+            origCol = Console.CursorLeft;
+            int amplitud, frecuencia;
+            int marginWave;
+            int nCasos,cont=0;
+            bool validacion1=true,validacion2=true,validacion3=true;
+            do
+            {
+                margin();
+                position(1, 1);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.WriteLine("         Ingrese los numero de casos:           ");
+                position(1, 2);
+                Console.BackgroundColor = ConsoleColor.Black;
+                nCasos =int.Parse(Console.ReadLine());//Ingresamos Datos de  los numero de casos que vamos a dibujar en la onda
+                if (nCasos > 0)
+                {
+                    validacion1 = false;
+                    while (cont != nCasos)
+                    {
+                        do
+                        {
+                            Console.Clear();
+                            margin();
+                            position(1, 1);
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.BackgroundColor = ConsoleColor.Blue;
+                            Console.WriteLine("                  Ingrese Amplitud:             ");
+                            position(1, 2);
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            amplitud = int.Parse(Console.ReadLine());//Ingresamos  datos de Amplitud
+                            if (amplitud > 0)
+                            {
+                                validacion2 = false;
+                                do
+                                {
+                                    Console.Clear();
+                                    margin();
+                                    position(1, 1);
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                    Console.BackgroundColor = ConsoleColor.Blue;
+                                    Console.WriteLine("            Ingrese Frecuencia:                 ");
+                                    position(1, 2);
+                                    Console.BackgroundColor = ConsoleColor.Black;
+                                    frecuencia = int.Parse(Console.ReadLine());//Ingresamos datos de Frecuencia
+                                    if (frecuencia > 0)
+                                    {
+                                        validacion3 = false;
+                                        Console.Clear();
+                                        position(1, 1);
+                                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                                        Console.BackgroundColor = ConsoleColor.White;
+                                        Console.WriteLine("Grafico N: " + (cont + 1));
+                                        position(1, 2);
+                                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                                        Console.BackgroundColor = ConsoleColor.White;
+                                        Console.WriteLine("Amplitud:" + amplitud + "   Frecuencia: " + frecuencia);
+                                        Console.BackgroundColor = ConsoleColor.Black;
+                                        marginWave = drawWave(frecuencia, amplitud) + 2;
+                                        Console.BackgroundColor = ConsoleColor.Black;
+                                        wavemargin(marginWave);//Dibujamos el margen donde se dibujara la onda y dibujamos la Onda
+                                        Console.BackgroundColor = ConsoleColor.Black;
+                                        Console.ReadKey();
+                                        cont++;
+                                    }
+
+                                } while (validacion3); //Validamos si son numeros positivos
+                            }
+
+                        } while (validacion2);//Validamos si son numeros positivos
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    position(60, 1);
+                    Console.WriteLine("------------------------------------");
+                    position(60, 2);
+                    Console.WriteLine("|Recuerde ingresar numeros positivos|");
+                    position(60, 3);
+                    Console.WriteLine("-------------------------------------");
+                }
+            }while (validacion1);//Validamos  si son numeros positivos
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
---
+++ Programa3P/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Programa3P
+{
+    class Program
+    {
+        protected static int origRow;
+        protected static int origCol;
+        protected static void position(int x, int y)//Nos coloca en una posicion de la consola y desde ese punto podemos escribir
+        {
+            try
+            {
+                Console.SetCursorPosition(origCol + x, origRow + y);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.Clear();
+                Console.WriteLine(e.Message);
+            }
+        }
+        static bool vocal(char c)//Devuelve un verdadero si es vocal y si no un falso 
+        {
+            switch (c)
+            {
+                case 'a':
+                    return true;
+                case 'e': 
+                    return true;
+                case 'i':
+                    return true;
+                case 'o':
+                    return true;
+                case 'u':
+                    return true;
+                case 'A':
+                    return true;
+                case 'E':
+                    return true;
+                case 'I':
+                    return true;
+                case 'O':
+                    return true;
+                case 'U':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            origRow = Console.CursorTop;
+            origCol = Console.CursorLeft;
+            String frase = string.Empty;
+            int tam;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            position(25, 0);
+            Console.WriteLine("-------------------------------------------------------");
+            position(25, 1);
+            Console.WriteLine("|               Ingrese una Frase                     |");
+            position(25, 2);
+            Console.WriteLine("-------------------------------------------------------");
+            position(25, 4);
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("->");
+            position(27, 4);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            frase = Console.ReadLine();//Ingresamos una frase 
+            String []arreglo= frase.Trim().Split(" ");//Borramos los espacios en branco del inicio y del final,dividimos la frase y guardamos en un arreglo de strings
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            position(25, 6);
+            Console.WriteLine("|                       PIG-LATIN                     |");
+            Console.BackgroundColor = ConsoleColor.Black;
+            position(45, 8);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            for (int i = 0; i < arreglo.Length; i++)//Recorremos nuestro arreglo de string
+            {
+                if (vocal(arreglo[i][0]))//Vamos comparando si existe una vocal o una consonante en la palabra
+                {
+                    Console.Write (arreglo[i] + "ay ");// Si es vocal colocamos al final de la palabra ay
+                }
+                else
+                {
+                    tam = arreglo[i].Length;
+                    Console.Write(arreglo[i].Substring(1)+arreglo[i][0]+"ay ");//Si es consonante cortamos la primera letra y lo colocamos al final y aumentamos ay
+                }
+            }
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            position(25, 10);
+            Console.WriteLine("|                                                     |");
+            Console.BackgroundColor = ConsoleColor.Black;
+
+        }
+    }
+}
